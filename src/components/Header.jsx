ebb09c6b4aedc9da44f69c3839f3572fb68ac472
Alethepata <alessia.patane@gmail.com>
2024-06-24 17:47:36 +0200

    add login / logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,31 @@
 import { NavLink } from "react-router-dom"
+import { useAuth } from "../contexts/AuthContext";
 
-function Header(){
+function Header() {
+  
+  const { logout, isLoggedIn } = useAuth();
+
   return(
     <header>
       <nav>
         <menu>
-          <li><NavLink to="/">Home</NavLink></li>
-          <li><NavLink to="/posts">Posts</NavLink></li>
+
+          <div>
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/posts">Posts</NavLink></li>
+          </div>
+
+          <div>
+            {
+              isLoggedIn == true &&
+              <li><button onClick={logout}>Log out</button></li>
+            }
+            {
+              isLoggedIn == false &&
+              <li><NavLink to="/login">Log In</NavLink></li>
+            }
+          </div>
+          
         </menu>
       </nav>
     </header>
